475f3803c30739ff66e61cba5f4491d85a926b74
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-04-26 13:35:14 +0300

    API URL updated

--- verysmartbackend/settings.py
+++ verysmartbackend/settings.py
@@ -136,6 +136,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://172.105.117.206:9889",
     "http://172.23.83.52:3000",
     "https://tuni-projects.github.io",
+    "https://hello.ibtehaz.xyz",
 ]
 
 CORS_ALLOW_METHODS = [
